f67330c2a7b190d0495afc944ccf7d2f92019744
King Li <jonathan.j.c.king.li@accenture.com>
2018-02-10 21:09:41 +0800

    updated schema

--- api/models/camper.js
+++ api/models/camper.js
@@ -3,61 +3,111 @@ var Schema = mongoose.Schema
 
 
 var CamperSchema = new Schema({
-  given_name: {
-    type: String
-  },
   first_name: {
     type: String,
     required: 'This field is required!'
   },
   nickname: {
     type: String,
+    required: 'This field is required!'
   },
   last_name: {
     type: String,
     required: 'This field is required!'
   },
-  full_name: {
+  middle_initial: {
     type: String
   },
   birthdate: {
-    type: Date
+    type: Date,
+    required: 'This field is required!'
   },
   gender: {
-    type: String
+    type: String,
+    required: 'This field is required!'
   },
   school: {
-    type: String
+    type: String,
+    required: 'This field is required!'
   },
   year: {
-    type: String
+    type: String,
+    required: 'This field is required!'
   },
   course: {
     type: String
   },
-  church: {
-    type: String
-  },
-  notes: {
-    type: String
-  },
   email: {
-    type: String
+    type: String,
+    required: 'This field is required!'
   },
   mobileno: {
-    type: String
+    type: String,
+    required: 'This field is required!'
   },
   facebook_id: {
     type: String
   },
-  disciplers: {
-    type: [Schema.Types.ObjectId]
+  shirt_size: {
+    type: String,
+    required: 'This field is required!'
   },
-  disciples: {
-    type: [Schema.Types.ObjectId]
+  church: {
+    type: String,
+    required: 'This field is required!'
+  },
+  gcf_member: {
+    type: Boolean,
+    required: 'This field is required!'
+  },
+  gcf_service: {
+    type: String
+  },
+  youth_group: {
+    type: Boolean,
+    required: 'This field is required!'
+  },
+  youth_group_leader: {
+    type: String
+  },
+  camp_first_time: {
+    type: Boolean,
+    required: 'This field is required!'
+  },
+  allergies: {
+    type: String
+  },
+  medications: {
+    type: String
+  },
+  note: {
+    type: String
+  },
+  father_name: {
+    type: String,
+    required: 'This field is required!'
+  },
+  father_birthday: {
+    type: Date,
+    required: 'This field is required!'
+  },
+  father_contactno: {
+    type: String,
+    required: 'This field is required!'
+  },
+  mother_name: {
+    type: String,
+    required: 'This field is required!'
+  },
+  mother_birthday: {
+    type: Date,
+    required: 'This field is required!'
+  },
+  mother_contactno: {
+    type: String,
+    required: 'This field is required!'
   }
-
-//  relationships: [ObjectId]
+  //  relationships: [ObjectId]
 });
 
 module.exports = mongoose.model('Camper', CamperSchema)
