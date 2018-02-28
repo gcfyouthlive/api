d4a95d8eb386322c3d5d47bec74f46419c0c682e
joshuagad <joshuagad@gmail.com>
2018-03-01 01:54:48 +0800

    Feature/authentication (#5)
    * authentication  attempt 1
    
    * Created middleware, temporarily worked but need to fix
    
    * working on localhost:3000

--- api/routes/campers.js
+++ api/routes/campers.js
@@ -1,32 +1,35 @@
-var express = require('express')
-var router = express.Router()
-var campers = require('../controllers/campers')
+var express = require('express');
+var router = express.Router();
+var campers = require('../controllers/campers');
+var credentials = require('../../config/credentials.json');
+
+const isAuthenticated = (req, res, next) => {
+  if (!req.isAuthenticated()) {
+    // if user is not logged in
+    res.redirect('/auth/login');
+  } else {
+    console.log(credentials.AUTHORIZED_IDS.indexOf(req.user.googleId));
+    if (credentials.AUTHORIZED_IDS.indexOf(req.user.googleId) >= 0) next();
+    else res.redirect('/auth/login');
+  }
+};
 
 router.use(function timeLog(req, res, next) {
   console.log('Time: ', Date.now(), ' ', req.url)
   next()
 });
 
+// Middleware
+router.use(isAuthenticated);
+
 router.route('/')
   .get(campers.get_all_campers)
-  .post(campers.add_a_camper)
+  .post(campers.add_a_camper);
 router.route('/:camperId')
   .get(campers.view_a_camper)
   .put(campers.update_a_camper)
-  .delete(campers.delete_a_camper)
+  .delete(campers.delete_a_camper);
 router.route('/:camperId/validation')
-  .get(campers.set_payment_true)
-// router.route('/:camperId/discipler')
-//   .put(campers.add_a_discipler)
-// router.route('/:camperId/discipler/:discipleId')
-//   .delete(campers.add_a_discipler)
-// router.route('/:camperId/meetups')
-//   .get(campers.get_camper_meetups)
-// router.route('/stats/age_demographic')
-//   .get(campers.get_age_demographic_stats)
-// router.route('/stats/year_demographic')
-//   .get(campers.get_year_demographic_stats)
-// router.route('/stats/age_demographic_d3')
-//   .get(campers.get_age_demographic_stats_d3)
+  .get(campers.set_payment_true);
 
-module.exports = router
+module.exports = router;
--- server.js
+++ server.js
@@ -1,30 +1,84 @@
-var express = require('express'),
-    app = express(),
-    port = process.env.PORT || "3000",
-    mongo_addr = process.env.MONGO_PORT_27017_TCP_ADDR || "127.0.0.1",
-    mongo_port = process.env.MONGO_PORT_27017_TCP_PORT || "27017",
-    mongoose = require('mongoose'),
-    Person = require('./api/models/person'),
-    Camper = require('./api/models/camper'),
-    Meetup = require('./api/models/meetup'),
-    bodyParser = require('body-parser'),
-    cors = require('cors');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || "3000";
+const mongo_addr = process.env.MONGO_PORT_27017_TCP_ADDR || "127.0.0.1";
+const mongo_port = process.env.MONGO_PORT_27017_TCP_PORT || "27017";
+const mongoose = require('mongoose');
+const Person = require('./api/models/person');
+const Camper = require('./api/models/camper');
+const Meetup = require('./api/models/meetup');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const cookieSession = require('cookie-session');
+const User = require('./api/models/user');
+const credentials = require('./config/credentials.json');
+
+const auth = require('./api/routes/auth')
+const people = require('./api/routes/people')
+const campers = require('./api/routes/campers')
+const meetups = require('./api/routes/meetups')
+const reports = require('./api/routes/reports')
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://'+mongo_addr+':'+mongo_port+'/youthlivedb');
+mongoose.connect('mongodb://' + mongo_addr + ':' + mongo_port + '/youthlivedb', () => {
+  console.log('connected to mongodb');
+});
 
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+passport.serializeUser((user, done) => {
+  console.log(user);
+  done(null, user.id);
+});
 
-var people = require('./api/routes/people')
-var campers = require('./api/routes/campers')
-var meetups = require('./api/routes/meetups')
-var reports = require('./api/routes/reports')
-app.use('/people', people)
+passport.deserializeUser((id, done) => {
+  console.log(id);
+  User.findById(id).then((user) => {
+    done(null, user);
+  })
+});
+
+passport.use(
+  new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID || credentials.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET || credentials.GOOGLE_CLIENT_SECRET,
+    callbackURL: credentials.OAUTH_CALLBACK
+  }, (accessToken, refreshToken, profile, done) => {
+    User.findOne({googleId: profile.id}).then((currentUser) => {
+      if (currentUser) {
+        console.log('user is: ', currentUser);
+        done(null, currentUser);
+      } else {
+        new User({
+          googleId: profile.id,
+          displayName: profile.displayName,
+          name: profile.name,
+          emails: profile.emails,
+          domain: profile.domain
+        }).save().then((newUser) => {
+          console.log('new user created: ', newUser);
+          done(null, newUser);
+        });
+      }
+    });
+  })
+);
+
+app.set('view engine', 'ejs');
+app.use(cookieSession({
+  maxAge: 6 * 60 * 60 * 1000,
+  keys: ['qwerty']
+}));
+
+// initialize passport and session
+app.use(passport.initialize());
+app.use(passport.session());
+
+// set up routes
+app.use('/auth', auth);
+
+// app.use('/people', people)
 app.use('/campers', campers)
-app.use('/meetups', meetups)
-app.use('/reports', reports)
+// app.use('/meetups', meetups)
+// app.use('/reports', reports)
 
 app.listen(port);
 
